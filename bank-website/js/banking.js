dac8dfcb5d3ed808bb2d6c981013ac7ba0a71fde
sazib <sazibkhancse@gmail.com>
2022-02-14 19:03:27 +0600

    Simple Interactive bank website

---
+++ bank-website/js/banking.js
@@ -0,0 +1,8 @@
+document.getElementById('deposit-submit').addEventListener('click', function () {
+
+
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmount = depositInput.value;
+    console.log("Deposit Amount Is :" + depositAmount);
+
+});
