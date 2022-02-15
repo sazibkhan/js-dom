64e5110a3bc555bdeee441a1507cc6bb5aee0968
sazib <sazibkhancse@gmail.com>
2022-02-15 18:19:28 +0600

    Bank Calculation Using Function

--- bank-calculation-using-function/js/banking.js
+++ bank-calculation-using-function/js/banking.js
@@ -1,11 +1,3 @@
-// function doubleIt(num) {
-//     const result = num * 2;
-//     return result;
-
-// }
-// const first = doubleIt(5);
-// const second = doubleIt(7);
-
 
 function getInputValue(inputId) {
     const inputField = document.getElementById(inputId);
@@ -27,25 +19,27 @@ function updateBalance(amount, isAdd) {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+
     if (isAdd == true) {
-        balanceTotal.innerText = previousBalanceTotal + depositAmount;
-        depositAmount;
+        balanceTotal.innerText = previousBalanceTotal + amount;
     } else {
-        balanceTotal.innerText = previousBalanceTotal - depositAmount;
-        depositAmount;
+        balanceTotal.innerText = previousBalanceTotal - amount;
+
     }
 }
 
 document.getElementById('deposit-button').addEventListener('click', function () {
     const depositAmount = getInputValue('deposit-input');
-    updateTotalField('deposit-total', depositAmount);
-    updateBalance(depositAmount, true);
-
+    if (depositAmount > 0) {
+        updateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
 });
 
 document.getElementById('withdraw-button').addEventListener('click', function () {
     const withdrawAmount = getInputValue('withdraw-input');
-    updateTotalField('withdraw-total', withdrawAmount);
-    updateBalance(withdrawAmount, false);
-
+    if (withdrawAmount > 0) {
+        updateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
 });
