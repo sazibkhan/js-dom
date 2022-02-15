87d4c4c5a22ba713491ac9ba599c80e1f7d7b214
sazib <sazibkhancse@gmail.com>
2022-02-15 08:18:53 +0600

    Simple Interactive bank website

--- bank-website/js/banking.js
+++ bank-website/js/banking.js
@@ -1,9 +1,9 @@
-document.getElementById('deposit-submit').addEventListener('click', function () {
+document.getElementById('deposit-btn').addEventListener('click', function () {
 
     const depositInput = document.getElementById('deposit-input');
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText);
-
+    //set Balance Total
     const totalDipisit = document.getElementById('total-dipisit');
     const previousDipisitText = totalDipisit.innerText;
     const previousDipisitAmount = parseFloat(previousDipisitText);
@@ -12,7 +12,7 @@ document.getElementById('deposit-submit').addEventListener('click', function () 
     totalDipisit.innerText = newTotalDeposit;
 
 
-    // update Account Balance
+    //Update Balance Total
     const totalBalance = document.getElementById('total-balance');
     const totalBalanceText = totalBalance.innerText;
     const previousTotalBalance = parseFloat(totalBalanceText);
@@ -20,15 +20,35 @@ document.getElementById('deposit-submit').addEventListener('click', function () 
     const newBlanceTotal = previousTotalBalance + newTotalDeposit;
     totalBalance.innerText = newBlanceTotal;
 
-
-
-
-
-
-
-
-
     //clear the diposit input field
     depositInput.value = "";
 
+});
+
+//handal withdrow even handler
+document.getElementById('withdrow-btn').addEventListener('click', function () {
+
+    const withdrowInput = document.getElementById('withdrow-input');
+    const withdrowAmountText = withdrowInput.value;
+    const newWithdrowAmount = parseFloat(withdrowAmountText);
+    //console.log(newWithdrowAmount);
+
+    //set withdrow Total
+    const totalWithdrow = document.getElementById('total-withdrow');
+    const previousWithdrowText = totalWithdrow.innerText;
+    const previousWithdrowAmount = parseFloat(previousWithdrowText);
+
+    const newTotalWithdrow = previousWithdrowAmount + newWithdrowAmount;
+    totalWithdrow.innerText = newTotalWithdrow;
+
+    //Update Balance Total
+    const totalBalance = document.getElementById('total-balance');
+    const previousBalanceText = totalBalance.innerText;
+    const previousTotalBalance = parseFloat(previousBalanceText);
+
+    const newBlanceTotal = previousTotalBalance - newTotalWithdrow;
+    totalBalance.innerText = newBlanceTotal;
+
+    //clear the diposit input field
+    withdrowInput.value = "";
 });
